66a462949d1e861e0b0eab74fb917043ca88824a
Kohyuckjoon <akhj123akhj@naver.com>
2024-12-26 01:16:56 +0900

    Button click method

--- app/src/main/java/com/example/chapter5/MainActivity.kt
+++ app/src/main/java/com/example/chapter5/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.chapter5
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.chapter5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val firstNumberText = StringBuilder("")
+    private val secondNumberText = StringBuilder("")
+    private val operatorText = StringBuilder("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun numberClicked(view : View) {
+        val numberString = (view as? Button)?.text?.toString() ?: ""
+        val numberText = if(operatorText.isEmpty()) firstNumberText else secondNumberText
+
+        numberText.append(numberString)
+        updateEquationTextView()
+    }
+
+    fun clearClicked(view : View) {
+        Log.e("clearClicked", "clear")
+    }
+
+    fun equalClicked(view : View) {
+        Log.e("clearClicked", "clear")
+    }
+
+    fun operatorClicked(view : View) {
+        Log.e("clearClicked", "clear")
+    }
+
+    private fun updateEquationTextView() {
+        binding.equationTextView.text = "$firstNumberText $operatorText $secondNumberText"
     }
 }
