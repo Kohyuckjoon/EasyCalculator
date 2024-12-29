9adf32d6e40ba7953eb29f009b614282df690e3d
Kohyuckjoon <akhj123akhj@naver.com>
2024-12-29 22:07:51 +0900

    예외처리 코드 추가, 연산 입력 구분(1,000) 컴마 추가

--- app/src/main/java/com/example/chapter5/MainActivity.kt
+++ app/src/main/java/com/example/chapter5/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.chapter5.databinding.ActivityMainBinding
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     private val firstNumberText = StringBuilder("")
     private val secondNumberText = StringBuilder("")
     private val operatorText = StringBuilder("")
+
+    private val decimalFormat = DecimalFormat("#,###")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,17 +37,57 @@ class MainActivity : AppCompatActivity() {
 
     fun clearClicked(view : View) {
         Log.e("clearClicked", "clear")
+        firstNumberText.clear()
+        secondNumberText.clear()
+        operatorText.clear()
+        updateEquationTextView()
+
+        binding.resultTextView.text = ""
     }
 
     fun equalClicked(view : View) {
         Log.e("clearClicked", "clear")
+        if(firstNumberText.isEmpty() || secondNumberText.isEmpty() || operatorText.isEmpty()) {
+            Toast.makeText(this, "올바르지 않은 수식입니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val firstNumber = firstNumberText.toString().toBigDecimal()
+        val secondNumber = secondNumberText.toString().toBigDecimal()
+
+        val result = when(operatorText.toString()) {
+            "+" -> decimalFormat.format(firstNumber + secondNumber)
+            "-" -> decimalFormat.format(firstNumber - secondNumber)
+
+            else -> {
+                "잘못된 수식입니다."
+            }
+        }.toString()
+
+        binding.resultTextView.text = result
     }
 
     fun operatorClicked(view : View) {
         Log.e("clearClicked", "clear")
+        val operatorString = (view as? Button)?.text?.toString() ?: ""
+
+        if(firstNumberText.isEmpty()) {
+            Toast.makeText(this, "먼저 숫자를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if(secondNumberText.isNotEmpty()) {
+            Toast.makeText(this, "1개의 연산자에 대해서만 연산이 가능합니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        operatorText.append(operatorString)
+        updateEquationTextView()
     }
 
     private fun updateEquationTextView() {
-        binding.equationTextView.text = "$firstNumberText $operatorText $secondNumberText"
+        val firstformattedNumber = if (firstNumberText.isNotEmpty()) decimalFormat.format(firstNumberText.toString().toBigDecimal()) else ""
+        val secondformattedNumber = if (secondNumberText.isNotEmpty()) decimalFormat.format(secondNumberText.toString().toBigDecimal()) else ""
+
+        binding.equationTextView.text = "$firstformattedNumber $operatorText $secondformattedNumber"
     }
 }
